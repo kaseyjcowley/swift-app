3ebc2bdafa5bdd991136d80c1b265661402289db
Kasey Cowley <kchunterdeluxe@gmail.com>
2016-05-06 00:18:50 -0600

    Add AddDebtViewController + cancel action

---
+++ AddDebtViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddDebtViewController.swift
+//  debt-payoff-swift
+//
+//  Created by Kasey Cowley on 5/5/16.
+//  Copyright © 2016 Kasey Cowley. All rights reserved.
+//
+
+import UIKit
+
+class AddDebtViewController: UIViewController {
+
+  // MARK: Properties
+  
+  // MARK: Navigation
+  @IBAction func cancel(sender: UIBarButtonItem) {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+}
